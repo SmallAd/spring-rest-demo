e4868bf3d6024b8bd7dc46cfd4bc3b9a773a28dd
SmallAd <vadim.piontkovskiy@gmail.com>
2020-06-13 21:15:43 +0300

    Micro fix

--- src/main/java/testgroup/springdemo/controller/StudentController.java
+++ src/main/java/testgroup/springdemo/controller/StudentController.java
@@ -2,7 +2,9 @@ package testgroup.springdemo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import testgroup.springdemo.model.Student;
@@ -15,14 +17,24 @@ import testgroup.springdemo.model.Student;
 @RequestMapping("/api")
 public class StudentController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("aaa", "111"));
         students.add(new Student("bbbb", "2222"));
         students.add(new Student("ccccc", "33333"));
-        
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
+    
+    @GetMapping("students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
+    }
+
 }
